5e945347a0eae43ad6115f071ed478efac1bf4dc
sergiosilva <sergio_silva@tecnologiasintech.com>
2017-05-18 20:30:14 -0700

    GuardiaMoveDIalogFragment Code Documentation and Refactorization

--- app/src/main/java/com/example/legible/seguridadargusapp/View/DialogFragment/GuardiaMoveDialogFragment.java
+++ app/src/main/java/com/example/legible/seguridadargusapp/View/DialogFragment/GuardiaMoveDialogFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.support.v4.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -31,77 +32,96 @@ import java.util.List;
 
 public class GuardiaMoveDialogFragment extends DialogFragment{
 
+    private static final String TAG = GuardiaMoveDialogFragment.class.getSimpleName();
+
     private Spinner spinner;
 
+    private static final String NOTIFICATION_ACTION = "MG";
+
+    // Firebase Reference(s)
+
+    // Clients inside database reference
     private DatabaseReference mRefCliente =
             FirebaseDatabase.getInstance().getReference().child("Argus").child("Clientes");
 
+    // Notification database reference
     private DatabaseReference mRefNotification =
             FirebaseDatabase.getInstance().getReference().child("Argus").child("Notificacion");
 
 
+    // Tempory Notification database reference
     private DatabaseReference mRefTmpNofication =
             FirebaseDatabase.getInstance().getReference().child("Argus").child("NotificacionTmp");
 
+    // Bitacora Registro datbase referece
+    // Used for creating one of the events, that tracks the progress of the current Supervisor
     private DatabaseReference mBitacoraRegistroRef =
-            FirebaseDatabase.getInstance().getReference().child("Argus").child("BitacoraRegistro").child(new DatePost().getDateKey()).child(ClienteRecyclerAdapter.mySupervisorKey);
+            FirebaseDatabase.getInstance().getReference().child("Argus").child("BitacoraRegistro")
+                    .child(new DatePost().getDateKey())
+                    .child(ClienteRecyclerAdapter.mySupervisorKey);
 
 
-    public static GuardiaMoveDialogFragment newInstance(String cliente,String supervisor,String guardiaKey,String guardiaNombre){
+    // Since Fragments need an empty constructor, if you wish to add parameters, you'll need to use newInstance
+
+    public static GuardiaMoveDialogFragment newInstance(
+            String cliente,String supervisor,String guardiaKey,String guardiaNombre){
+
+        // Instanciate the Object
         GuardiaMoveDialogFragment fragment = new GuardiaMoveDialogFragment();
 
+        // Create a new Bundle
         Bundle args = new Bundle();
+
+        // Save the components for later use
         args.putString("supervisorNombre",supervisor);
         args.putString("clienteNombre",cliente);
         args.putString("guardiaKey",guardiaKey);
         args.putString("guardiaNombre",guardiaNombre);
+
+        // Set the arguments
         fragment.setArguments(args);
 
+        // Return Fragment
         return fragment;
     }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
+        // Builds the dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Mover Guardia");
 
+        // Inflate the view(s)
         LayoutInflater inflater = LayoutInflater.from(getContext());
         final View view = inflater.inflate(R.layout.dialog_fragment_guardia_move,null);
+        spinner = (Spinner) view.findViewById(R.id.spinnerCliente);
 
-        mRefCliente.addValueEventListener(new ValueEventListener() {
-            final List<String> clientes = new ArrayList<String>();
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+        // Set the spinner with all the clients inside the database
+        setSpinner();
 
-                for (DataSnapshot ds: dataSnapshot.getChildren()){
-                    String Cliente = ds.child("clienteNombre").getValue(String.class);
-                    if (!Cliente.equals(ClienteRecyclerAdapter.myCliente))
-                    clientes.add(Cliente);
-                }
-
-                spinner = (Spinner) view.findViewById(R.id.spinnerCliente);
-                ArrayAdapter<String> clientesAdapter = new ArrayAdapter<String>(getActivity(),R.layout.spinner_item,clientes);
-                clientesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinner.setAdapter(clientesAdapter);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
+        // Set the view inside the dialog
         builder.setView(view);
 
-        builder.setPositiveButton("Anadir", new DialogInterface.OnClickListener() {
+        // data is uploaded when the 'anadir' button is clicked.
+        builder.setPositiveButton(R.string.button_anadir, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                pushData();
+
+                // Uploads the details handeled on the fragment
+                uploadData();
+
+                // TODO: Adds Guard to the new Client
+
+
+                // TODO: Deletes Guard inside the current Client
+
+
             }
         });
 
-        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+        // Current Dialog gets removed when 'Cancelar' Button is clicked.
+        builder.setNegativeButton(R.string.button_cancelar, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
@@ -111,49 +131,95 @@ public class GuardiaMoveDialogFragment extends DialogFragment{
         return builder.create();
     }
 
-    private void pushData(){
+    private void uploadData(){
+
+        // Creates an object which will store the values for later use
+        GuardiaMoveBasicInfo guardiaMoveBasicInfo = new GuardiaMoveBasicInfo(
+                        getArguments().getString("guardiaKey"), // Guardia Key
+                        getArguments().getString("clienteNombre"), // Clientes Name
+                        spinner.getSelectedItem().toString()); // Client selected on spinner
 
 
-        GuardiaMoveBasicInfo guardia = new GuardiaMoveBasicInfo();
-        guardia.setGuardiaKey(getArguments().getString("guardiaKey"));
-        guardia.setGuardiaClienteActual(getArguments().getString("clienteNombre"));
-        guardia.setGuardiaClienteNuevo(spinner.getSelectedItem().toString());
-
-        //Get Action
-        String accion = "MG";
-        //Get Current Date
-        DatePost date = new DatePost();
-        String currentDate = date.getDatePost();
-
-        //GetCurrent Supervisor
-        //Todo set the correct SuperVisor
-        String supervisor = getArguments().getString("supervisorNombre");
-
-        //setDescription
-        //Todo set the guardia that was moved
-        String guardiaNombre = getArguments().getString("guardiaNombre");
-        String description = supervisor + " movio a "+ guardiaNombre+"(Guardia)";
-
-        Notificacion notificacion = new Notificacion(accion,description,currentDate);
-
-
-
+        // Creates and saves a random key for reference
         String key = mRefNotification.push().getKey();
 
+        // Creates an object which will store the values for later use
+        Notificacion notificacion = new Notificacion(
+                // Action which describes notification
+                NOTIFICATION_ACTION,
+                // Description
+                String.format("%s movio  %s a %s",
+                        ClienteRecyclerAdapter.mySupervisor, // Gets the current Supervisor
+                        getArguments().getString("guardiaNombre"), // Get the current Guardia
+                        spinner.getSelectedItem().toString() // Gets the new client
+                        ),
+                new DatePost().getDatePost()); // gets the current Date
+
+        // Notification Value set
         mRefNotification.child(key).setValue(notificacion);
-        mRefNotification.child(key).child("informacion").setValue(guardia);
+        mRefNotification.child(key).child("informacion").setValue(guardiaMoveBasicInfo);
 
+        // Tempory Notification value set
         mRefTmpNofication.child(key).setValue(notificacion);
-        mRefTmpNofication.child(key).child("informacion").setValue(guardia);
+        mRefTmpNofication.child(key).child("informacion").setValue(guardiaMoveBasicInfo);
 
+        // Bitacora Registro value set
         mBitacoraRegistroRef.child(key).setValue(notificacion);
-        mBitacoraRegistroRef.child(key).child("informacion").setValue(guardia);
+        mBitacoraRegistroRef.child(key).child("informacion").setValue(guardiaMoveBasicInfo);
 
-        Toast.makeText(getContext(), "Su solicitud se ha enviado con exito",Toast.LENGTH_LONG).show();
-
-
-
+        // Demonstrates that the data uploaded was succes.
+        Toast.makeText(getContext(), R.string.solicitud_enviada,Toast.LENGTH_LONG).show();
 
 
+    }
+
+    private void setSpinner(){
+
+        // Value Event Listener used for collecting all the current clients inside datebase
+        mRefCliente.addValueEventListener(new ValueEventListener() {
+
+            // Creates an empty list of Clients
+            final List<String> clientes = new ArrayList<String>();
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                // Iterates through all clientes from database
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+
+                    // Gets the property(s) of the client, in this case the clients name.
+                    String Cliente = ds.child("clienteNombre").getValue(String.class);
+
+                    //  Makes sure the current client doesn't get included inside the list.
+                    if (!Cliente.equals(ClienteRecyclerAdapter.myCliente)){
+
+                        // Adds the Client inside list of Clientes.
+                        clientes.add(Cliente);
+                    }
+
+
+                }
+
+                // Creates a String Array in which collects all the clients
+                ArrayAdapter<String> clientesAdapter = new ArrayAdapter<String>(getActivity(),R.layout.spinner_item,clientes);
+
+                // Set the spinner adapter
+                clientesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinner.setAdapter(clientesAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+                // Firebase Error
+                Log.e(TAG, databaseError.toString());
+                Toast.makeText(
+                        GuardiaMoveDialogFragment.this.getContext(),
+                        R.string.GuardiaMoveDialog_Error_Loading_Clients,
+                        Toast.LENGTH_LONG)
+                        .show();
+
+            }
+        });
     }
 }
